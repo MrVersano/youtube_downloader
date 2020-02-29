b7b16439209a9ff478c3029b65f26febe35506af
Kobi Versano <kobi.versano@Kobis-MacBook-Pro.local>
2020-02-29 13:34:04 +0200

    Added ability to download YT video

--- downloader/views.py
+++ downloader/views.py
@@ -1,3 +1,50 @@
+from __future__ import unicode_literals
+from django.http import HttpResponse
 from django.shortcuts import render
+import youtube_dl
+import os
+
+STATIC_URL = 'downloader/static/'
 
 # Create your views here.
+def home(request):
+    # Remove all mp3 files in static folder
+    file_list = os.listdir(STATIC_URL)
+    for file in file_list:
+        os.remove(os.path.join(STATIC_URL, file))
+
+
+    # Get the YouTube video if a URL was submitted
+    if request.GET.get('yturl'):
+        yturl = request.GET.get('yturl')
+    
+        ydl_opts = {
+        'nocheckcertificate': True,
+        'format': 'bestaudio/best',
+        'forcetitle': True,
+        'outtmpl': f'{STATIC_URL}%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+        
+
+        url = yturl
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            filename = f"{ydl.extract_info(url, download=False)['title']}.mp3"
+            print(f"Filename: {filename}.mp3")
+            ydl.download([url])
+        
+        # fsock = open(f'{STATIC_URL}{filename}', 'r')
+        # response = HttpResponse(fsock, content_type='audio/mpeg')
+        # response['Content-Disposition'] = f"attachment; filename={STATIC_URL}{filename}"
+        # return response
+        with open(f'{STATIC_URL}{filename}', 'rb') as mp3:
+            response = HttpResponse(mp3.read())
+            response['content_type'] = 'audio/mpeg'
+            response['Content-Disposition'] = f'attachment;filename={filename}'
+            return response
+
+    return render(request, 'downloader/home.html')
--- reference/youtube_downloader.py
+++ reference/youtube_downloader.py
@@ -1,15 +1,21 @@
 from __future__ import unicode_literals
 import youtube_dl
+import os
 
 
 ydl_opts = {
     'nocheckcertificate': True,
     'format': 'bestaudio/best',
+    # 'forcetitle': True,``
+    'outtmpl': '/Users/kobi.versano/Downloads/%(title)s.%(ext)s',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
 }
+
+url = 'https://www.youtube.com/watch?v=QNJL6nfu__Q'
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=QNJL6nfu__Q'])
+    ydl.download([url])
+    
