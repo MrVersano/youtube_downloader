36a6fab6fbe65c8e44321935d66d20779659d47d
Kobi Versano <kobi.versano@Kobis-MacBook-Pro.local>
2020-02-29 11:31:24 +0200

    initial commit

---
+++ reference/youtube_downloader.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+ydl_opts = {
+    'nocheckcertificate': True,
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=QNJL6nfu__Q'])
